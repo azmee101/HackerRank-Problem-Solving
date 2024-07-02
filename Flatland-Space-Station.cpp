62959bb1c5df1cd93c3a4e49b51da79a1c7fca51
Azmee <iazmee101@gmail.com>
2024-07-02 23:56:40 +0600

    #include <bits/stdc++.h>

--- Flatland-Space-Station.cpp
+++ Flatland-Space-Station.cpp
@@ -28,28 +28,42 @@ int main()
         int tmp, indx;
 
         if(it != v.end()){
+            
             indx = distance(v.begin(), it);
 
-            tmp = v[indx];
-            if(indx > 0) indx--;
-            tmp = min(tmp, v[indx]);
+            tmp = v[indx]; // immediate big
+            
+            if(indx > 0){
+                
+                indx--; // immediate small
+
+                int x = i - v[indx], y = tmp - i;
+
+                tmp = x >= y ? v[indx+1] : v[indx];
+            }
+            
         }
         else{
-            tmp = v[0];
+
+            tmp = v[c-1];
         }
 
         auto jt = upper_bound(v.begin(), v.end(), i);
         int tmp2;
         
         if(jt != v.end()){
+            
             indx = distance(v.begin(), jt);
             tmp2 = v[indx];
         }
         else{
+
             tmp2 = v[c-1];
         }
+
         mx = max(mx, min(abs(i-tmp), abs(i-tmp2)));
     }
 
     cout << mx << '\n';
+    return 0;
 }
